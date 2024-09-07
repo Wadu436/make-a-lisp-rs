a5a176d13a2ab1f459c23319939988a6854ad293
Warre Dujardin <warre.dujardin@gmail.com>
2024-09-08 00:23:15 +0200

    step 0

---
+++ src/bin/cli.rs
@@ -0,0 +1,35 @@
+use color_eyre::Result;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
+use std::io::{self, Write};
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let mut rl = DefaultEditor::new()?;
+
+    loop {
+        // Read the input
+        let input = rl.readline("user> ");
+        match input {
+            Ok(line) => {
+                // Save the input in the history
+                rl.add_history_entry(line.as_str())?;
+
+                // Process the line
+                let output = make_a_lisp_rs::rep(line);
+
+                // Print the output
+                println!("{}", output);
+            }
+            Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+pub fn read(input: String) -> String {
+    input
+}
+
+pub fn eval(input: String) -> String {
+    input
+}
+
+pub fn print(input: String) -> String {
+    input
+}
+
+pub fn rep(input: String) -> String {
+    print(eval(read(input)))
+}
